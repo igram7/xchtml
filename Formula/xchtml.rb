52394012b5c2e26a12ba66677ea80d6451f812d1
Gram <eganeshram7@gmail.com>
2026-06-17 00:27:24 +0530

    Update local formula reference for v1.0.3

--- Formula/xchtml.rb
+++ Formula/xchtml.rb
@@ -3,8 +3,8 @@ class Xchtml < Formula
 
   desc "Generate beautiful HTML reports from Xcode .xcresult bundles"
   homepage "https://github.com/igram7/xchtml"
-  url "https://github.com/igram7/xchtml/archive/refs/tags/v1.0.2.tar.gz"
-  sha256 "db542246f024f385b046d474b3d6b08f8c90390e7880a3e5188e559e3a3876e0"
+  url "https://github.com/igram7/xchtml/archive/refs/tags/v1.0.3.tar.gz"
+  sha256 "2e74e82af88ef45ecf35fa23d41d087e6bf8df4c86e2b19448faba6076e05ac0"
   license "MIT"
 
   depends_on "python@3.12"
